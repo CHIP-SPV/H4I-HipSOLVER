12b4dacf54f0ed10398ceaf0653f5ea9352397bd
Philip C. Roth <rothpc@ornl.gov>
2023-07-19 18:13:26 +0000

    Fix bug on header installation dependent on hipSOLVER version.

--- Scripts/install-hipsolver-header.sh
+++ Scripts/install-hipsolver-header.sh
@@ -8,14 +8,9 @@ srcdir=$1
 bindir=$2
 instdir=$3
 
-mkdir -p $bindir/include
-mkdir -p $bindir/include/internal
-cp $srcdir/library/include/hipsolver.h $bindir/include/
-cp $srcdir/library/include/internal/hipsolver-types.h $bindir/include/internal
-cp $srcdir/library/include/internal/hipsolver-functions.h $bindir/include/internal
-cp $srcdir/library/include/internal/hipsolver-compat.h $bindir/include/internal
-cp $srcdir/library/include/internal/hipsolver-refactor.h $bindir/include/internal
+cp -r $srcdir/library/include $bindir
 
 mkdir -p $instdir/share/licenses
 cp $srcdir/LICENSE.md $instdir/share/licenses/hipsolver.h.LICENSE.md
 cp $srcdir/LICENSE.md $instdir/share/licenses/hipsolver-types.h.LICENSE.md
+
