6eb8f7ab0216928790b517b47330a740bfb14804
Philip C. Roth <rothpc@ornl.gov>
2023-07-19 18:14:39 +0000

    Update to use shared handle manager from MKLshim.

--- src/util.cpp
+++ src/util.cpp
@@ -2,43 +2,34 @@
 #include "hip/hip_runtime.h"
 #include "hip/hip_interop.h"
 #include "h4i/mklshim/mklshim.h"
+#include "h4i/hiputils/HandleManager.h"
 
-hipsolverStatus_t hipsolverCreate(hipsolverHandle_t* handle){
-  if(handle != nullptr)
-  {
-    // HIP supports mutile backends hence query current backend name
-    auto backendName = hipGetBackendName();
-    // Obtain the handles to the back handlers.
-    unsigned long handles[4];
-    int           nHandles = 4;
-    hipGetBackendNativeHandles((uintptr_t)NULL, handles, &nHandles);
-    *handle = H4I::MKLShim::Create(handles, nHandles, backendName);
-  }
-  return (*handle != nullptr) ? HIPSOLVER_STATUS_SUCCESS : HIPSOLVER_STATUS_HANDLE_IS_NULLPTR;
+static H4I::HIPUtils::HandleManager<hipsolverHandle_t,
+                                    hipsolverStatus_t,
+                                    HIPSOLVER_STATUS_SUCCESS,
+                                    HIPSOLVER_STATUS_HANDLE_IS_NULLPTR> hmgr;
+
+hipsolverStatus_t
+hipsolverCreate(hipsolverHandle_t* handle)
+{
+    return hmgr.Create(handle);
 }
 
-hipsolverStatus_t hipsolverDestroy(hipsolverHandle_t handle){
-  if(handle != nullptr)
-  {
-    H4I::MKLShim::Context* ctxt = static_cast<H4I::MKLShim::Context*>(handle);
-    H4I::MKLShim::Destroy(ctxt);
-  }
-  return (handle != nullptr) ? HIPSOLVER_STATUS_SUCCESS : HIPSOLVER_STATUS_HANDLE_IS_NULLPTR;
+hipsolverStatus_t
+hipsolverDestroy(hipsolverHandle_t handle)
+{
+    return hmgr.Destroy(handle);
 }
 
-hipsolverStatus_t hipsolverSetStream(hipsolverHandle_t handle,
-                                     hipStream_t       stream) {
-  if(handle != nullptr)
-  {
-      H4I::MKLShim::Context* ctxt = static_cast<H4I::MKLShim::Context*>(handle);
-
-      int nHandles = H4I::MKLShim::nHandles;
-      std::array<uintptr_t, H4I::MKLShim::nHandles> nativeHandles;
-      //todo: add return error check
-      hipGetBackendNativeHandles(reinterpret_cast<uintptr_t>(stream),
-              nativeHandles.data(), &nHandles);
-
-      H4I::MKLShim::SetStream(ctxt, nativeHandles);
-  }
-  return (handle != nullptr) ? HIPSOLVER_STATUS_SUCCESS : HIPSOLVER_STATUS_HANDLE_IS_NULLPTR;
+hipsolverStatus_t
+hipsolverSetStream(hipsolverHandle_t handle, hipStream_t stream)
+{
+    return hmgr.SetStream(handle, stream);
 }
+
+hipsolverStatus_t
+hipsolverGetStream(hipsolverHandle_t handle, hipStream_t* stream)
+{
+    return hmgr.GetStream(handle, stream);
+}
+
