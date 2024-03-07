3785b068c41bb0ccbc64aaf80c45b6106a17431a
Paulius Velesko <pvelesko@pglc.io>
2024-03-07 11:21:05 +0200

    make backwards compat

--- src/util.cpp
+++ src/util.cpp
@@ -6,12 +6,22 @@
 hipsolverStatus_t hipsolverCreate(hipsolverHandle_t* handle){
   if(handle != nullptr)
   {
+    #ifndef hipGetBackendName
     // Obtain the handles to the back handlers.
     int nHandles;
     hipGetBackendNativeHandles((uintptr_t)NULL, 0, &nHandles);
     unsigned long handles[nHandles];
     hipGetBackendNativeHandles((uintptr_t)NULL, handles, 0);
     *handle = H4I::MKLShim::Create(handles, nHandles);
+    #else 
+    // HIP supports mutile backends hence query current backend name
+    auto backendName = hipGetBackendName();
+    // Obtain the handles to the back handlers.
+    unsigned long handles[4];
+    int           nHandles = 4;
+    hipGetBackendNativeHandles((uintptr_t)NULL, handles, &nHandles);
+    *handle = H4I::MKLShim::Create(handles, nHandles, backendName);
+    #endif
   }
   return (*handle != nullptr) ? HIPSOLVER_STATUS_SUCCESS : HIPSOLVER_STATUS_HANDLE_IS_NULLPTR;
 }
@@ -34,9 +44,13 @@ hipsolverStatus_t hipsolverSetStream(hipsolverHandle_t handle,
       int nHandles = H4I::MKLShim::nHandles;
       std::array<uintptr_t, H4I::MKLShim::nHandles> nativeHandles;
       //todo: add return error check
+      #ifndef hipGetBackendName
       hipGetBackendNativeHandles(reinterpret_cast<uintptr_t>(stream),
               nativeHandles.data(), 0);
-
+      #else
+      hipGetBackendNativeHandles(reinterpret_cast<uintptr_t>(stream),
+              nativeHandles.data(), &nHandles);
+      #endif
       H4I::MKLShim::SetStream(ctxt, nativeHandles);
   }
   return (handle != nullptr) ? HIPSOLVER_STATUS_SUCCESS : HIPSOLVER_STATUS_HANDLE_IS_NULLPTR;
